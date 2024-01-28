e3ea3ca2c0b8f3c0430a3f00a9d8d3a139b5cdb0
ravisharma <ravi.neo02@gmail.com>
2024-01-27 21:41:06 -0500

    Initial commit.

---
+++ src/main/java/myappjava/AppApplication.java
@@ -0,0 +1,13 @@
+package myappjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
